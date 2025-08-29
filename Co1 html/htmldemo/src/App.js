f054ba09e0476f7c625c353748d3a2f9317cd428
2400030015 <2400030015@kluniversity.in>
2025-08-29 09:35:35 +0530

    my latest changes

---
+++ Co1 html/htmldemo/src/App.js
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <strong>Welcome</strong><br/>
+      <i>Welocme</i><br/>
+      <b>Welcome</b ><br/>
+      <ins>Welcome</ins> <br/>
+      <del>Welcome</del><br/>
+      <em>Welocme</em><br/>
+     <h1>Welcome to <sup>React</sup></h1>
+     <h1>Welcome to <sub>React</sub></h1>
+     <mark>Welocme</mark>
+     <h2>welcome to the React with npx</h2>
+     <p>Here ,I'm ready to develop react project using npx and react framework</p>
+     <p>So,we can create framework</p>
+     </div>
+  );
+}
+
+export default App;
+
+
---
+++ javascript/first.js
@@ -0,0 +1,16 @@
+console.log("Welcome to JAVA session");
+let x=10;
+let  y=20;
+const pi=3.14;
+console.log(pi);
+console.log(x);
+console.log(y);
+let d="Dundi";
+console.log(d);
+
+x=9;
+y=8;
+let sum=x+y;
+console.log(x);
+console.log(y);
+console.log("Sum:"+sum);
